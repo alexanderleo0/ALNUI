8fdc82076d5e1691159669045261c96640f1b05b
alexanderleo0 <alnikitin3@gmail.com>
2023-03-21 23:07:55 +0300

    Finish with rick and morty api

--- Sources/ALNUI/ALNUI.swift
+++ Sources/ALNUI/ALNUI.swift
@@ -8,33 +8,34 @@ import SwiftUI
 import Kingfisher
 
     //public struct SimpleCard <ALNUIImage> : View where ALNUIImage: View{
-public struct SimpleCard: View{
+public struct SimpleCard : View{
 
     
-    private let image: KFImage
+    private let image: Image
     private let text: Text
     
-    public init(image: KFImage, text: Text) {
+    public init(image: Image, text: Text) {
         self.image = image
         self.text = text
     }
     
     public var body: some View {
         ZStack(alignment: .center) {
-            image
-                .resizable()
-                .scaledToFill()
-                .frame(width: 200, height: 200)
+            Group{
+                image
+                    .resizable()
+                    .scaledToFill()
+                 
+                Color.black
+                    .opacity(0.4)
+            }
+                .frame(width: 150, height: 150)
                 .clipped()
-            Color.black
-                .opacity(0.4)
             text
                 .font(.title)
                 .bold()
                 .foregroundColor(.white)
-                .frame(maxWidth: 180)
-                
-                
+                .frame(maxWidth: 130)
         }
         .cornerRadius(20)
     }
