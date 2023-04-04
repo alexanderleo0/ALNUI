08d7d86082c66a8f2e2fe68983e7f48f517abd34
alexanderleo0 <alnikitin3@gmail.com>
2023-04-04 11:12:52 +0300

    1

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ALNUI",
-            dependencies: []),
+            dependencies: ["Kingfisher"]),
         .testTarget(
             name: "ALNUITests",
             dependencies: ["ALNUI"]),
